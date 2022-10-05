a5538a41342334ef33132337819d9d875be29d84
unknown <MED210080@spengergasse.at>
2022-10-05 12:19:13 +0200

    ExProperties

---
+++ ExProperties/Lehrer.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace ExProperties
+{
+    public class Lehrer
+    {
+        public string Vorname { get; set; } = "";
+        public string? Zuname { get; set; }
+        public decimal? Bruttogehalt
+        {
+            get => _bruttogehalt; 
+            set { if(_bruttogehalt == null)
+                    _bruttogehalt = value;}
+        }
+        decimal? _bruttogehalt = null;
+
+        public string Kuerzel { get => Zuname?.Substring(0,3).ToUpper()?? ""; }
+
+        public decimal Nettogehalt { get => (Bruttogehalt * 0.8M) ?? 0M; }    
+        
+
+
+    }
+}
---
+++ ExProperties/Rechteck.cs
@@ -0,0 +1,39 @@
+﻿
+namespace ExProperties
+{
+    public class Rechteck
+    {
+        public int Laenge{
+            get => _laenge;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Ungültige Länge");
+                else
+                    _laenge = value;
+                    CalculateFlaeche();
+            }
+        }
+        int _laenge;
+
+        public int Breite{
+            get => _breite;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Ungültige Länge");
+                else
+                    _breite = value;
+                    CalculateFlaeche ();
+            }
+        }
+        int _breite;
+        public int Flaeche { get => _flaeche;}
+        int _flaeche;
+
+        private void CalculateFlaeche()
+        {
+            _flaeche = _breite * _laenge;
+        }
+    }
+}
